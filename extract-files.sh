LineageOS/android_device_xiaomi_sdm845-common

sdm845-common: Add shim for missing symbol in lib-imsvt.so
Fixes:

    E ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: cannot locate symbol "_ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb" referenced by "/system/product/lib64/lib-imsvideocodec.so"...

Change-Id: I4364a1e1da2ae2120194f44c233f1f8b1cd0f7ac

extract-files.sh
@@ -41,6 +41,9 @@ function blob_fixup() {
         system_ext/lib64/libdpmframework.so)
             sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"
             ;;
+        product/lib64/lib-imsvideocodec.so)
+            ${PATCHELF} --add-needed "lib-imsvtshim.so" "${2}"
+            ;;
     esac
 }
 

Dump-Stuffs/device_xiaomi_beryllium

Signed-off-by: Cruzer1 <preetamswain143@gmail.com>

extract-files.sh
@@ -12,6 +12,9 @@ function blob_fixup() {
     vendor/lib/camera/components/com.qti.node.watermark.so)
         patchelf --add-needed "libpiex_shim.so" "${2}"
         ;;
+    system_ext/lib64/lib-imsvideocodec.so)
+        ${PATCHELF} --add-needed "lib-imsvtshim.so" "${2}"
+        ;;
     esac
 }
 